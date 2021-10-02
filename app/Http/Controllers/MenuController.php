e2b2170eaeb4fea1e41700c7de2f1c4316d9e9d2
Rezas Siahcheshm <reza.siahcheshm@gmail.com>
2021-10-02 03:35:25 +0330

    made dynamic restaurant page

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cafe;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class MenuController extends Controller
 {
-    public function index()
+    public function index(Cafe $cafe)
     {
+//        return $cafe->products()->get();
+//return Cafe::query()->whereId('1')->get();
         $data = [
             'page_name' => 'restaurant' ,
             'category_name' => 'none' ,
+            'cafe' => $cafe ,
+            'products' => $cafe->products()->get(),
         ];
-        $this->seo()->setTitle('رستوران');
+        $this->seo()->setTitle('رستوران'.' '.$cafe->name);
         return view('home.pages.restaurant')->with($data);
     }
 }
