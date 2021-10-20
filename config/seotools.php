04aed8ccf6c080c6609c3b41764e3197df990361
Rezas Siahcheshm <reza.siahcheshm@gmail.com>
2021-10-20 20:48:25 +0330

    change title website

--- config/seotools.php
+++ config/seotools.php
@@ -9,7 +9,7 @@ return [
          * The default configurations to be used by the meta generator.
          */
         'defaults'       => [
-            'title'        => "منوپی", // set false to total remove
+            'title'        => "دیجی شام", // set false to total remove
             'titleBefore'  => false, // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
             'description'  => 'منوی دیجیتال', // set false to total remove
             'separator'    => ' | ',
