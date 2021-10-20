a34bcac61a156ee7f4ebb7a40a3013f3989a96f0
Rezas Siahcheshm <reza.siahcheshm@gmail.com>
2021-10-20 20:47:49 +0330

    change products icon

--- resources/views/admin/inc/sidebar.blade.php
+++ resources/views/admin/inc/sidebar.blade.php
@@ -73,7 +73,7 @@
                 <li class="menu {{isActive(['admin.products.index','admin.products.create','admin.products.edit'])}}">
                     <a href="#product" data-toggle="collapse" aria-expanded="{{isActive(['admin.products.index','admin.products.create','admin.products.edit'],'true','false')}}" class="dropdown-toggle">
                         <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-coffee"><path d="M18 8h1a4 4 0 0 1 0 8h-1"></path><path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path><line x1="6" y1="1" x2="6" y2="4"></line><line x1="10" y1="1" x2="10" y2="4"></line><line x1="14" y1="1" x2="14" y2="4"></line></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-bag"><path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path><line x1="3" y1="6" x2="21" y2="6"></line><path d="M16 10a4 4 0 0 1-8 0"></path></svg>
                             <span>محصولات</span>
                         </div>
                         <div>
