6722632eeb390481d1fd9ad10b1f178b20699da7
Rezas Siahcheshm <reza.siahcheshm@gmail.com>
2021-11-04 05:39:55 +0330

    Implement cafe information

--- app/Http/Controllers/InformationController.php
+++ app/Http/Controllers/InformationController.php
@@ -2,15 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cafe;
 use Illuminate\Http\Request;
 
 class InformationController extends Controller
 {
-    public function index()
+    public function index(Cafe $cafe)
     {
         $data = [
             'page_name' => 'information' ,
             'category_name' => 'none' ,
+            'cafe' => $cafe ,
         ];
         $this->seo()->setTitle('اطلاعات');
         return view('home.pages.information')->with($data);
