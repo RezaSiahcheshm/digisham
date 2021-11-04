5be6d2454b1d6eb323f12ccacf86960547f58a91
Rezas Siahcheshm <reza.siahcheshm@gmail.com>
2021-11-04 05:46:56 +0330

    fixed issue

--- resources/views/home/inc/styles.blade.php
+++ resources/views/home/inc/styles.blade.php
@@ -5,7 +5,6 @@
 <link rel="stylesheet" href="/css/style.css">
 <link rel="stylesheet" href="/css/bootstrap-rtl.css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<script src="https://kit.fontawesome.com/2d624b7c4d.js" crossorigin="anonymous"></script>
 <!-- END GLOBAL MANDATORY STYLES -->
 
 <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
