1240a500cd844a70941d7070816cf9a65f281a3a
Rezas Siahcheshm <reza.siahcheshm@gmail.com>
2021-11-04 05:37:48 +0330

    Implement user profile

--- routes/web/home.php
+++ routes/web/home.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\InformationController;
 use App\Http\Controllers\MenuController;
-use App\Http\Controllers\SlideMenuController;
+use App\Http\Controllers\ProfileController;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,19 +22,29 @@ use App\Http\Controllers\SlideMenuController;
 
 
 Route::get('/' , function () {
-//    return \Carbon\Carbon::now();
+    auth()->loginUsingId(1);
+    return jdate()->format('y');
 
+//    $user=User::find('1');
+//    return $user->permissions()->get();
+//    return auth()->loginUsingId(1);
 //    auth()->logout();
-    return redirect(route('home'));
+//    if(Gate::allows('edit-user')){
+//        return 'yes';
+//    }
+//    return 'no';
+//    return redirect(route('home'));
 //    return view('emails.login-to-website');
 });
 Route::get('home' , [HomeController::class , 'index'])->name('home');
 Route::get('cafe/{cafe}' , [MenuController::Class , 'index'])->name('cafe');
 Route::get('comment' , [CommentController::Class , 'index'])->name('comment');
-Route::get('information' , [InformationController::Class , 'index'])->name('information');
+Route::get('cafe/information/{cafe}' , [InformationController::Class , 'index'])->name('information');
 
 Route::middleware('auth')->group(function () {
-    Route::get('slideMenu' , [SlideMenuController::Class , 'index'])->name('slideMenu');
+    Route::get('profile' , [ProfileController::Class , 'index'])->name('profile.index');
+    Route::get('profile/edit' , [ProfileController::Class , 'edit'])->name('profile.edit');
+    Route::patch('profile' , [ProfileController::Class , 'update'])->name('profile.update');
 });
 
 Route::get('login' , [LoginController::class , 'showLoginForm'])->name('login');
